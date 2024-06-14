bdaae97b7ac062f4d24f763849c401853548fef2
OASIS <s2422008@stu.musashino-u.ac.jp>
2024-06-14 09:14:56 +0900

    update db and expect DS_Store, .venv.

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+def get_delay_percentage():
+	dbname = 'saikyo_line_delay_history.db'
+	conn = sqlite3.connect(dbname)
+	for row in conn.execute('SELECT COUNT(*) FROM first_seven WHERE delay_time > 0;'):
+		print(row[0])
+	conn.close()
+
+get_delay_percentage()
